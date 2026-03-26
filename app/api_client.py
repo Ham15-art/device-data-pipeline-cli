36894f25a12791028129f27355e6af25e04f07bc
Hamza <hamza.tuto15@gmail.com>
2026-03-26 13:24:55 +0100

    api_client: added duration of calling

--- app/api_client.py
+++ app/api_client.py
@@ -1,7 +1,9 @@
 import aiohttp
 import asyncio
 import logging
+import time
 
+start = time.perf_counter()
 logger = logging.getLogger(__name__)
 
 # protects API from overload when implemented in line 14
@@ -19,18 +21,31 @@ async def fetch_device_info(session, device_id):
             async with session.get(url) as response:
                 response.raise_for_status()
                 data = await response.json()
+                
+            duration = time.perf_counter() - start
 
-                logger.debug(f"Device {device_id} response received")
+            logger.debug(f"Device {device_id} completed in {duration:.3f}s")
 
-                # returns dict
-                return {"device_id": device_id, "data": data, "error": None}
+            # returns a coroutine
+            return {
+                "device_id": device_id, 
+                "data": data, 
+                "error": None
+                }
 
         except Exception as e:
+            duration = time.perf_counter() - start
+
             logger.error(
-                f"failed to fetch device {device_id} (url={url}): {e}", exc_info=True
+                f"failed to fetch device {device_id} (url={url}) after {duration:.3f}s: {e}", 
+                exc_info=True
             )
-            # returns dict
-            return {"device_id": device_id, "data": None, "error": str(e)}
+            # returns coroutine
+            return {
+                "device_id": device_id, 
+                "data": None, 
+                "error": str(e)
+                }
 
 
 async def fetch_all_devices(device_ids):
@@ -38,11 +53,11 @@ async def fetch_all_devices(device_ids):
     timeout = aiohttp.ClientTimeout(total=5)
 
     async with aiohttp.ClientSession(timeout=timeout) as session:
-
+        #each task is a coroutine object, not yet executed/run
         tasks = [fetch_device_info(session, device_id) for device_id in device_ids]
 
         logger.info(f"Starting API enrichment for {len(device_ids)} devices...")
-
+        #execute all coroutines concurrently and collect results (list of dicts)
         results = await asyncio.gather(*tasks)
 
         # log summary for Api calling
